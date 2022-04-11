e97aa2217308e8bbef6be6904a1172bf3a94aaf7
emourany <enzo.mourany@etu.u-bordeaux.fr>
2022-04-11 17:29:16 +0200

    added drawCard method

--- CardGame2022/CardView.cs
+++ CardGame2022/CardView.cs
@@ -12,12 +12,26 @@ namespace CardGame2022
         private Point Position;
         private int card;
         private int malus;
+        // DrawCard proprieties
+        public Size cardSize;
+        public Color colorCard = Color.Red;
+        public Font fontCardText = new Font("Poppins", 12.0f);
+        public SolidBrush brushCard = new SolidBrush(Color.White);
         
         internal CardView(Point _Position, int _card)
         {
             this.Position = _Position;
             this.card = _card;
-            malus = CardsHandling.MalusValue(this.card);
+            this.malus = CardsHandling.MalusValue(this.card);
+        }
+
+        public void DrawCard(int card, Graphics g)
+        {
+            String text = card.ToString();
+            Rectangle rect = new Rectangle(this.Position, cardSize);
+            Pen p = new Pen(colorCard);
+            g.DrawRectangle(p, rect);
+            g.DrawString(text, fontCardText, brushCard, Position);
         }
     }
 }
