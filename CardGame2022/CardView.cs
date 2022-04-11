201d93c50b83e75dd48b5311ced4f8032887f16f
emourany <enzo.mourany@etu.u-bordeaux.fr>
2022-04-11 16:57:54 +0200

    added CardView method and constructor

---
+++ CardGame2022/CardView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame2022
+{
+    internal class CardView
+    {
+        private Point Position;
+        private int card;
+        private int malus;
+        
+        internal CardView(Point _Position, int _card)
+        {
+            this.Position = _Position;
+            this.card = _card;
+            malus = CardsHandling.MalusValue(this.card);
+        }
+    }
+}
